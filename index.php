ba7ff40522fc09f6e489007a15eddabe915e0d74
Rafael Ferrete <rafaelferrete333@gmail.com>
2024-05-23 19:53:55 -0300

    update

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Herois</title>
+</head>
+<body>
+    <div>
+        <h1></h1>
+        <form action="">
+            <label for="">Digite o universo do herói:</label>
+            <input type="text" name="" id="">
+            <input type="submit" value="Mostar">
+        </form>
+    </div>
+    <?php
+     
+    ?>
+</body>
+</html>
